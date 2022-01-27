ebf7a3db62dda5e7918e81df4909ff164158a849
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-01-27 19:42:10 +0200

    Fix linter errors

--- src/addScore.js
+++ src/addScore.js
@@ -1,10 +1,10 @@
-import { data } from './useAPI.js'
+import { data } from './useAPI.js';
+
 export class Leaderboard {
   constructor() {
     this.submit = document.querySelector('.submit');
     this.inputName = document.querySelector('.name');
     this.inputScore = document.querySelector('.score');
-    this.listScores = document.querySelector('.list-scores');
     this.refresh = document.querySelector('.refresh');
   }
 
@@ -26,45 +26,35 @@ export class Leaderboard {
     });
   }
 
-  showRecentScores = (array) => {
-    array.sort((a,b) => b.score - a.score);
-    array.forEach((element) => {
-      const li = document.createElement('li');
-      li.className = ('list-item');
-      li.innerText = `${element.user}: ${element.score}`;
-      this.listScores.appendChild(li);
-    });
+  showErrorMessage = () => {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = ('Please enter a valid score');
+    errorMessage.className = ('error-message');
+    this.inputScore.parentNode.appendChild(errorMessage);
   }
 
-    showErrorMessage = () => {
-      const errorMessage = document.createElement('p');
-      errorMessage.textContent = ('Please enter a valid score');
-      errorMessage.className = ('error-message');
-      this.inputScore.parentNode.appendChild(errorMessage);
-    }
+  removeErrorMessage = () => {
+    const errorMessage = document.querySelector('.error-message');
+    this.inputScore.parentNode.removeChild(errorMessage);
+  }
 
-    removeErrorMessage = () => {
-      const errorMessage = document.querySelector('.error-message');
-      this.inputScore.parentNode.removeChild(errorMessage);
-    }
+  showValidationMessage = () => {
+    const message = document.createElement('p');
+    message.textContent = ('Please fill all the boxes');
+    message.className = ('message');
+    this.inputScore.parentNode.appendChild(message);
+  }
 
-    showValidationMessage = () => {
-      const message = document.createElement('p');
-      message.textContent = ('Please fill all the boxes');
-      message.className = ('message');
-      this.inputScore.parentNode.appendChild(message);
-    }
+  removeValidationMessage = () => {
+    const message = document.querySelector('.message');
+    this.inputScore.parentNode.removeChild(message);
+  }
 
-    removeValidationMessage = () => {
-      const message = document.querySelector('.message');
-      this.inputScore.parentNode.removeChild(message);
-    }
-
-    refreshList = () => {
-      this.refresh.addEventListener('click', () => {
-        window.location.reload();
-      });
-    }
+  refreshList = () => {
+    this.refresh.addEventListener('click', () => {
+      window.location.reload();
+    });
+  }
 }
 
 export const newScore = new Leaderboard();
--- src/useAPI.js
+++ src/useAPI.js
@@ -1,26 +1,36 @@
-import { newScore } from './addScore.js';
 export class Data {
-    constructor() {
-        this.URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kPwgOmu44kog43JsIlkN/scores/';
-    }
+  constructor() {
+    this.URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kPwgOmu44kog43JsIlkN/scores/';
+    this.listScores = document.querySelector('.list-scores');
+  }
 
     fetchData = async () => {
-        const response = await fetch(this.URL);
-        const data = await response.json();
-        newScore.showRecentScores(data.result);
-      };
+      const response = await fetch(this.URL);
+      const data = await response.json();
+      this.showRecentScores(data.result);
+    };
 
-      postScoresToAPI = async (newName, newScore) => {
-        await fetch(this.URL, {
-          method: 'POST',
-          body: JSON.stringify({
-            user: newName,
-            score: newScore
-          }),
-          headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
-        });
-      };
+    postScoresToAPI = async (newName, newScore) => {
+      await fetch(this.URL, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: newName,
+          score: newScore,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+    };
+
+    showRecentScores = (array) => {
+      array.sort((a, b) => b.score - a.score);
+      array.forEach((element) => {
+        const li = document.createElement('li');
+        li.className = ('list-item');
+        li.innerText = `${element.user}: ${element.score}`;
+        this.listScores.appendChild(li);
+      });
+    }
 }
 export const data = new Data();
